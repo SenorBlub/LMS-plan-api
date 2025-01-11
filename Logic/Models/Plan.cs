a55e98163657e103f63db2b8fb76184f898f40a6
SenorBlub <thomasverhappen@Gmail.com>
2025-01-11 21:40:00 +0100

    plan nullable properties added

--- Logic/Models/Plan.cs
+++ Logic/Models/Plan.cs
@@ -9,8 +9,8 @@ namespace Logic.Models
 	public class Plan
 	{
 		public Guid Id { get; set; }
-		public string Title { get; set; }
-		public string Description { get; set; }
+		public string? Title { get; set; }
+		public string? Description { get; set; }
 		public bool Finished { get; set; }
 		public ICollection<PlanActivity>? PlanActivities { get; set; }
 		public ICollection<PlanDate>? PlanDates { get; set; }
