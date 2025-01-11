cb3dba4cb572071f5acbf51992659ba3b8401f05
SenorBlub <thomasverhappen@Gmail.com>
2025-01-11 18:30:53 +0100

    Error they all have patch

--- LMS-plan-api/Program.cs
+++ LMS-plan-api/Program.cs
@@ -19,7 +19,8 @@ builder.Services.AddCors(options =>
 {
 	options.AddDefaultPolicy(corsPolicyBuilder =>
 	{
-		corsPolicyBuilder.WithOrigins(Env.GetString("FRONT_END_URL"))
+		corsPolicyBuilder.WithOrigins()
+			.AllowAnyOrigin()
 			.AllowAnyHeader()
 			.AllowAnyMethod()
 			.AllowCredentials()
@@ -28,7 +29,7 @@ builder.Services.AddCors(options =>
 });
 
 var connectionString =
-	$"Server={Env.GetString("DB_HOST")};Database={Env.GetString("DB_NAME")};User Id={Env.GetString("DB_USER")};Password={Env.GetString("DB_PASSWORD")};";
+	$"Server={Env.GetString("DB_HOST")};Port={Env.GetString("DB_PORT")};Database={Env.GetString("DB_NAME")};User={Env.GetString("DB_USER")};Password={Env.GetString("DB_PASSWORD")};";
 
 builder.Services.AddDbContext<PlanDbContext>(options =>
 	options.UseMySql(
